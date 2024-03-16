eb434f5f89f744a6701a9001ad0988e2d454ccf4
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-03-16 22:17:53 +0100

    chg: database management moved from utils to its own folder

---
+++ src/database/__init__.py
@@ -0,0 +1,3 @@
+"""The Database package is used to query the database."""
+from database.database_manager import DatabaseManager
+from utils.paths import DATABASE_PATH, SQL_PATH_FILES, TABLES_SQL_PTH
