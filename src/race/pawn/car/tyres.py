062479dfcff256c5d6aed11c39c41bbf43509c57
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-03-17 13:59:14 +0100

    chg: minor changes on Tyres class !minor

--- src/race/pawn/car/tyres.py
+++ src/race/pawn/car/tyres.py
@@ -10,16 +10,16 @@ from utils.constant import FPS
 @dataclass(init=False)
 class TyreType:
     """The TyreType stores the attributes of a tyre based on its type: soft, medium or hard."""
-    
+
     def __init__(self, weather: Weather, type_id: int):
         data = DBManager.get_data_by_id(type_id, 'tyre_type')
         self.clean_perfo_coeff = data['clean_perfo_coeff']
         self.zero_perfo_coeff = data['zero_perfo_coeff']
-        self.perfo_drop_state = data['perfo_drop_state']-(0.1*(weather.humidity))
-        self.perfo_at_drop = data['perfo_at_drop']-(0.1*(weather.humidity))
-        self.degradation_coeff = data['degradation_coeff']*(weather.temperature-0.5)**0.5
+        self.perfo_drop_state = data['perfo_drop_state']-0.1*(weather.humidity)
+        self.perfo_at_drop = data['perfo_at_drop']-0.1*(weather.humidity)
+        self.degradation_coeff = data['degradation_coeff']*(0.95+weather.temperature*0.05)
 
-class Tyre:
+class Tyres:
     """The Tyre instances represents the pawn set of 4 tyres during a race."""
 
     def __init__(self, tyre_type: Literal['Hard', 'Medium', 'Soft', 1,2,3], weather: Weather) -> None:
@@ -29,8 +29,8 @@ class Tyre:
             tyre_type = {'Hard':1,'Medium':2,'Soft':3}[tyre_type]
         self.type = TyreType(weather, tyre_type)
         self.flat = False # True if the tyre is flat, when no more state or when flattened by a card.
-    
-    def get_perfo_coefficient(self, state):
+
+    def get_perfo_coeff(self, state: float):
         """Calculate the performance coefficient of the tyre."""
         return affine2(
             x=state, 
@@ -39,7 +39,7 @@ class Tyre:
             sep_x=self.type.perfo_drop_state, 
             sep_y=self.type.perfo_at_drop
         )
-    
-    def degrade(self):
+
+    def degrade(self, tyre_management_perfo_coeff: float):
         """Degrade the tyres."""
-        self.state -= self.type.degradation_coeff/FPS
+        self.state -= self.type.degradation_coeff/FPS*tyre_management_perfo_coeff
