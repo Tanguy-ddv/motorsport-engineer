47c1336e1260d0c59baa96758e63f1405710d385
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-03-21 08:44:30 +0100

    new: definition of the sound mixer and the input manager

---
+++ src/engine/mixer.py
@@ -0,0 +1,31 @@
+"""The mixer is used to mix music and sounds."""
+import pygame.mixer as mx
+
+class Mixer:
+    """The mixer is used to mix music and sounds."""
+
+    def __init__(self):
+        self.__music_loop_start = None
+        self.__sounds: dict[str, mx.Sound] = {}
+
+    def play_music(self, path, music_loop_start):
+        """Load a music."""
+        mx.music.load(path)
+        mx.music.play(-1)
+        self.__music_loop_start = music_loop_start
+
+    def update(self):
+        """Do all the action done every loop."""
+        if not mx.music.get_busy():
+            mx.music.set_pos(self.__music_loop_start)
+        
+    def load_sound(self, sound_name: str, sound_path: str):
+        """Load a sound to be played during the game."""
+        self.__sounds[sound_name] = mx.Sound(sound_path)
+    
+    def play_sound(self, sound_name: str):
+        """Play a sound when an action is triggered."""
+        if sound_name in self.__sounds:
+            self.__sounds[sound_name].play()
+        else:
+            print(f"try to play the sound {sound_name} but is not loaded.")
