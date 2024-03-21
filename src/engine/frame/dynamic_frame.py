2699591eafa1d8c66de2dcd06f3acbbc8cf0f8b7
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-03-21 10:41:10 +0100

    fix: made possible to load already zoomed images.

--- src/engine/frame/dynamic_frame.py
+++ src/engine/frame/dynamic_frame.py
@@ -1,8 +1,10 @@
 
 from pygame import Surface
 import pygame.transform as tf
+import pygame.image as im
 from .frame import Frame
 from pygame.font import Font
+from typing import Literal
 
 
 def __zoom_image(image: Surface, zoom_level) -> Surface:
@@ -13,9 +15,20 @@ def __zoom_image(image: Surface, zoom_level) -> Surface:
 class DynamicFrame(Frame):
     """A Frame where we can zoom, put alpha and rotate component."""
 
-    def __init__(self, background_path: str, font: Font, zoom_level: float):
+    def __init__(self, background_path: str, font: Font, zoom_level: float, zoom_background_path: str=None):
+        """
+        Create a dynamic frame.
+
+        brackground_path: The path to the background image.
+        font: the pygame.font.Font object to display text. If set to None, no text can be displayed on this frame.
+        zoom_level: a float representing the level of zoom.
+
+        """
         super().__init__(background_path, font)
+        # There is two versions of the window, the zoomed one and the normal.
         self._zoom_level = zoom_level
+        if zoom_background_path is not None:
+            self._zoom_background = im.load(zoom_background_path) # In case the image with a better resolution already exists.
         self._zoom_background = __zoom_image(self._background, zoom_level)
         self.zoom_window = Surface(self._zoom_background.get_size())
     
@@ -38,11 +51,23 @@ class DynamicFrame(Frame):
             orientation: float,
             alpha: float,
             zoom: bool,
+            zoom_image: bool = True
         ):
-        """Blit an image on the screen."""
+        """
+        Blit an image on the screen.
+    
+        image_name : the name of the image previously loaded.
+        position: the position of the center of the image.
+        orientation: the orientation of the image, must be float in radians.
+        alpha: The alpha component of the image to make transparent.
+        zoom: bool. If true, the image is bilt on the zoom window, else on the normal window.
+        zoom_image: If true, the image bilt on the zoom background is the zoom image, else it is the normal one.
+        use zoom_image = False when the image loaded is made only to be plotted on the zoom image, and the size corresponds.
+        """
         if zoom:
-            image_name = image_name + 'zoom'
             position = position[0]*self._zoom_level, position[1]*self._zoom_level
+            if zoom_image:
+                image_name = image_name + 'zoom'
         image = self._images[image_name]
         image_tr = tf.rotate(image, orientation)
         image_tr.set_alpha(alpha)
