11da3e8c06c68e1f61be43d4a5118bd0813f08ed
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-03-21 13:44:05 +0100

    chg: removal of input manager. Inputs know managed with a method of the engine.

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,8 +1,10 @@
 """The engine is used to manage inputs, sounds and displays."""
 import pygame
+import pygame.event as ev
+from pygame import mouse, key
+from pygame import MOUSEBUTTONDOWN, KEYDOWN, QUIT
 from .mixer import Mixer
 from .screen import Screen
-from .input_manager import InputManager
 
 class Engine:
     """An engine is a object used to manage inputs, sounds and to display the image."""
@@ -13,8 +15,6 @@ class Engine:
 
         self.screen = Screen(width, height, caption)
         self.mixer = Mixer()
-        self.input_manager= InputManager()
-
         self.__wait_time = int(1000/fps) # [ms] the waiting time at the end of each frame.
 
     def __del__(self):
@@ -26,10 +26,15 @@ class Engine:
         self.screen.update()
         self.mixer.update()
         pygame.time.wait(self.__wait_time)
-
-    def user_quit(self) -> bool:
-        """Verify if the player want to leave the game."""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return True
-        return False
+    
+    def get_input(self) -> dict:
+        """Get the user input."""
+        inputs = {'mouse' : [], 'key' : [], 'quit' : False}
+        for event in ev.get():
+            if event.type == MOUSEBUTTONDOWN:
+                inputs['mouse'].append((event.button, mouse.get_pos()))
+            if event.type == KEYDOWN:
+                inputs['key'].append(key.name(event.key))
+            if event.type == QUIT:
+                inputs['quit'] = True
+        return inputs
